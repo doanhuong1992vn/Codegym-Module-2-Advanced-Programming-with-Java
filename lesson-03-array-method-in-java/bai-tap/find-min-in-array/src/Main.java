90e516a017ab2ae9a8c812b3a29ace7058459fb7
doanhuong1992vn <doanhuong1992vn@gmail.com>
2023-03-01 00:09:37 +0700

    update lesson 3

---
+++ lesson-03-array-method-in-java/bai-tap/find-min-in-array/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int size = input.nextInt();
+        int[] array = new int[size];
+        for(int i = 0; i < size; i++) {
+            System.out.printf("Enter element %d: ", i);
+            array[i] = input.nextInt();
+        }
+        int min = array[0];
+        System.out.print("Your array: ");
+        for(int i = 0; i < size; i++) {
+            System.out.print(array[i] + "\t");
+            if(min > array[i]) {
+                min = array[i];
+            }
+        }
+        System.out.printf("\nThe smallest element in array is %d", min);
+    }
+}
