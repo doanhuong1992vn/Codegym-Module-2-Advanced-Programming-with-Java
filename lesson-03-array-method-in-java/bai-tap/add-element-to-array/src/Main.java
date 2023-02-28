3c7de801a6f7885267f9dd725ab9475363993cbf
doanhuong1992vn <doanhuong1992vn@gmail.com>
2023-02-28 23:13:17 +0700

    update lesson 3

--- lesson-03-array-method-in-java/bai-tap/add-element-to-array/src/Main.java
+++ lesson-03-array-method-in-java/bai-tap/add-element-to-array/src/Main.java
@@ -3,10 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[10];
+        System.out.print("Element in source array: ");
         for (int i = 0; i < array.length; i++) {
             array[i] = i * 2;
+            System.out.print(array[i] + "\t");
         }
         Scanner input = new Scanner(System.in);
-        System.out.print("");
+        System.out.print("\nEnter element to insert: ");
+        int element = input.nextInt();
+        System.out.print("Enter index to insert element: ");
+        int index = input.nextInt();
+        if (index < 0 || index >= array.length) {
+            System.out.print("Unable to insert element into array!");
+        } else {
+            for (int i = array.length -1; i > index; i--) {
+                array[i] = array[i - 1];
+            }
+            array[index] = element;
+            System.out.print("Element in new array: ");
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i] + "\t");
+            }
+        }
     }
 }
---
+++ lesson-03-array-method-in-java/bai-tap/merge-array/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //nhập dữ liệu mảng thứ nhất:
+        System.out.print("Enter size of the first array: ");
+        int firstSize = input.nextInt();
+        int[] firstArray = new int[firstSize];
+        for (int i = 0; i < firstSize; i++) {
+            System.out.printf("Enter element %d: ", i);
+            firstArray[i] = input.nextInt();
+        }
+        //in ra mảng thứ nhất:
+        System.out.print("Element in the first array: ");
+        for(int i = 0; i < firstSize; i++) {
+            System.out.print(firstArray[i] + "\t");
+        }
+        //nhập dữ liệu mảng thứ 2:
+        System.out.print("\nEnter size of the second array: ");
+        int secondSize = input.nextInt();
+        int[] secondArray = new int[secondSize];
+        for (int i = 0; i < secondSize; i++) {
+            System.out.printf("Enter element %d: ", i);
+            secondArray[i] = input.nextInt();
+        }
+        //in ra mảng thứ 2:
+        System.out.print("Element in the second array: ");
+        for(int i = 0; i < secondSize; i++) {
+            System.out.print(secondArray[i] + "\t");
+        }
+        //tạo mảng thứ 3:
+        int thirdSize = firstSize + secondSize;
+        int[] thirdArray = new int[thirdSize];
+        //gán các phần tử mảng 1 vào mảng 3:
+        System.out.print("\nElement in the third array: ");
+        for (int i = 0; i < firstSize; i++) {
+            thirdArray[i] = firstArray[i];
+            System.out.print(thirdArray[i] + "\t");
+        }
+        //khai báo biến index = chiều dài mảng 1 để làm chỉ mục bắt đầu khi gán phần tử mảng 2 vào mảng 3
+        int index = firstSize;
+        for (int i = 0; i < secondSize; i++, index++) {
+            thirdArray[index] = secondArray[i];
+            System.out.print(thirdArray[index] + "\t");
+        }
+
+    }
+}
