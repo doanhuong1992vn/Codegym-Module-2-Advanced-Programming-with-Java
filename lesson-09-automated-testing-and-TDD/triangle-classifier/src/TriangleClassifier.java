47dac1c11db04a9b2f02028934398d0ad80c20fd
doanhuong1992vn <doanhuong1992vn@gmail.com>
2023-03-05 22:25:15 +0700

    update lesson 09

--- lesson-09-automated-testing-and-TDD/triangle-classifier/src/TriangleClassifier.java
+++ lesson-09-automated-testing-and-TDD/triangle-classifier/src/TriangleClassifier.java
@@ -1,6 +1,31 @@
 public class TriangleClassifier {
     public static String classifyTriangle(double aSide, double bSide, double cSide) {
-        throw new UnsupportedOperationException();
+        if (isTriangle(aSide, bSide, cSide)) {
+            if (isIsoscelesTriangle(aSide, bSide, cSide)) {
+                if (isEquilateralTriangle(aSide, bSide, cSide)) {
+                    return "The equilateral triangle";
+                } else {
+                    return "The isosceles triangle";
+                }
+            } else {
+                return  "The normal triangle";
+            }
+        } else {
+            return "Not a triangle";
+        }
     }
 
+    private static boolean isEquilateralTriangle(double aSide, double bSide, double cSide) {
+        return aSide == bSide && aSide == cSide;
+    }
+
+    private static boolean isIsoscelesTriangle(double aSide, double bSide, double cSide) {
+        return aSide == bSide || aSide == cSide || bSide == cSide;
+    }
+
+    private static boolean isTriangle(double aSide, double bSide, double cSide) {
+        if (aSide > 0 && bSide > 0 && cSide > 0) {
+            return aSide + bSide > cSide && aSide + cSide > bSide && bSide + cSide > aSide;
+        } else return false;
+    }
 }
