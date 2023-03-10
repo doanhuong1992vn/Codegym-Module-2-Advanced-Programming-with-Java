c7fcd0e7d1aa88f71cc8d52d3c14d4ea10c11e7d
doanhuong1992vn <doanhuong1992vn@gmail.com>
2023-03-11 00:11:01 +0700

    update

---
+++ lesson-11-list-stack-queue/src/queue_linkedlist/Node.java
@@ -0,0 +1,9 @@
+package queue_linkedlist;
+
+public class Node {
+    int key;
+    Node next;
+    Node(int key){
+        this.key = key;
+    }
+}
---
+++ lesson-11-list-stack-queue/src/queue_use_circle_linkedlist/Node.java
@@ -0,0 +1,9 @@
+package queue_use_circle_linkedlist;
+
+public class Node {
+    public int data;
+    Node link;
+    Node(int data){
+        this.data = data;
+    }
+}
